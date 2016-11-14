33d844db8cbbb5ad1bf643ccb6600c271db9eb2c
PierreN <pierre.nicolai.06@gmail.com>
2016-11-14 11:20:41 +0100

    Mindwave real time testing
    Adding a debug UI that can be enabled/disabled for testing purposes +
    MindwaveInterface starts at the begining of the Game.

---
+++ Assets/Scripts/UI/DebugUIManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugUIManager : MonoBehaviour {
+
+    public Image fillImage;
+    public Text meditationValue;
+
+	// Use this for initialization
+	void Start () {
+        fillImage.fillAmount = 0.5f;
+        meditationValue.text = "50";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Mindwave value : " + MindwaveInterface.attentionValue);
+        fillImage.fillAmount = (float)MindwaveInterface.meditationValue / 100;
+        meditationValue.text = MindwaveInterface.meditationValue.ToString();
+	}
+}
