aa23138cb4cfe920044ea8dcd86e7a3d138d9a35
PierreNicolai <pierre.nicolai.06@gmail.com>
2017-02-17 21:32:10 +0100

    Adding new lustre chain
    New activable chain mecanism to kill woots in kazhad dum

--- Assets/Scripts/Interactables/StatueController.cs
+++ Assets/Scripts/Interactables/StatueController.cs
@@ -3,9 +3,11 @@
 public class StatueController : Target {
 
     public GameObject Lustre;
-    
+    public GameObject chaine;
+        
     public override void OnShot()
     {
         Lustre.GetComponent<Animator>().Play("Fall");
+        Destroy(chaine);
     }
 }
