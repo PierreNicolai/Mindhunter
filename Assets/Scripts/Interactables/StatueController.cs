bcc5aac7522188c5129167be87515dede9477982
PierreNicolai <pierre.nicolai.06@gmail.com>
2017-02-22 01:46:51 +0100

    Changes described below
    - Adding a lustre in kazhad-dum post collapse, using unity physics, that
    will be used to kill the three woots in kazhad dum
    - Adding shrooms
    - Navmesh baked, with dynamic obstacles (like collapse)

--- Assets/Scripts/Interactables/StatueController.cs
+++ Assets/Scripts/Interactables/StatueController.cs
@@ -1,13 +1,27 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class StatueController : Target {
 
     public GameObject Lustre;
     public GameObject chaine;
+
+    public GameObject[] Woots;
         
     public override void OnShot()
     {
-        //Lustre.GetComponent<Animator>().Play("Fall");
+        StartCoroutine(LustreFall());
+    }
+
+    private IEnumerator LustreFall()
+    {
+        List<Transform> lustreSubObjects = Lustre.GetComponentsInChildren<Transform>().ToList();
+        lustreSubObjects.Remove(Lustre.transform);
+        lustreSubObjects.ForEach(lso => lso.GetComponent<Rigidbody>().isKinematic = false);
         Destroy(chaine);
+        yield return new WaitForSeconds(1f);
+        //Kill woots
     }
 }
