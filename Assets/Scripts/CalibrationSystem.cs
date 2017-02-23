6cdde8e2a2b1197a1ce0126e20cdafb0e37995aa
PierreN <pierre.nicolai.06@gmail.com>
2017-02-23 12:26:10 +0100

    First algorithm of calibration system

---
+++ Assets/Scripts/CalibrationSystem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using MindHunter.Managers;
+
+public class CalibrationSystem : MonoBehaviour
+{
+    void Update()
+    {
+        print("Value : " + MindwaveInterface.Instance.attentionValue);
+    }
+}
