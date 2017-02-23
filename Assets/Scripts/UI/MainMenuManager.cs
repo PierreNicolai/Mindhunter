70aeb5ff9be7f5de35595ae4d544e7a8df9c906e
PierreNicolai <pierre.nicolai.06@gmail.com>
2017-02-23 21:54:54 +0100

    Modifications on menu. All buttons except "Credits" work

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+
+	public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Jungle");
+    }
+}
