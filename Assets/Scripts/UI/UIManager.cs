895b385e05935afee2ea8e87732b6134133bfa48
PierreNicolai <pierre.nicolai.06@gmail.com>
2017-02-26 00:31:57 +0100

    Integration of dynamic aim

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -7,11 +7,22 @@ public class UIManager : PersistentSingleton<UIManager>
     private Animator _anim;
 
     public Image fillBar;
+    public GameObject Sight;
    
     void Start()
     {
         _anim = GetComponent<Animator>();
         InvokeRepeating("UpdateMindwaveUI", 0f, 3f);
+    }
+
+    void Update()
+    {
+        float scale = 0.05f * MindwaveInterface.Instance.attentionValue;
+        Sight.GetComponent<RectTransform>().transform.localScale = new Vector3(scale, scale, scale);
+        Color currentSightColor = Sight.GetComponent<Image>().color;
+        float alpha = MindwaveInterface.Instance.attentionValue * (7 / 1000) + 0.3f;
+        Color newColor = new Color(1f, 1f, 1f, alpha);
+        currentSightColor = newColor;
     }
     
     public void UpdateMindwaveUI()
